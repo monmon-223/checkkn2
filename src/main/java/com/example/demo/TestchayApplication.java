e5f361194c03a63724af0719ffc52281f2e18595
monmon-223 <datdtps41034@gmail.com>
2025-01-12 19:30:26 +0700

    qư

---
+++ src/main/java/com/example/demo/TestchayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestchayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestchayApplication.class, args);
+	}
+
+}
